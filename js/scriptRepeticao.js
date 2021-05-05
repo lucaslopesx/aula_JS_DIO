1536da2edbf249ef07b4d929f5d21842e4f3d667
Lucas Lopes <68665689+lucaslopesx@users.noreply.github.com>
2021-05-05 13:25:30 -0300

    Create scriptRepeticao.js

---
+++ js/scriptRepeticao.js
@@ -0,0 +1,15 @@
+/*
+var idade = prompt("qual a sua idade? ")
+
+if (idade >= 18) {
+    alert("Maior de idade")
+}
+else{
+    alert("Menor de idade")
+}
+*/
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
