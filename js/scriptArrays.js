f02e87c50630806c614e266226003c966109b4a7
Lucas Lopes <68665689+lucaslopesx@users.noreply.github.com>
2021-05-05 13:16:09 -0300

    updates

---
+++ js/scriptArrays.js
@@ -0,0 +1,20 @@
+var nome = "Lucas Lopes"
+var idade = 19
+var idade2 = 25
+var frase = "Japão é o melhor time"
+
+/*alert(frase.replace("Japão", "Brasil"))
+alert(frase.toLowerCase())*/
+
+var lista = ["apple", "strawberry", "orange"]
+lista.push("grape")
+// lista.pop()
+// lista.pop()
+// console.log(lista.join(","))
+/*
+var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta)
+*/
+
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas[1])
--- script.js
+++
@@ -1,7 +0,0 @@
-var nome = "Lucas Lopes"
-var idade = 19
-var idade2 = 25
-var frase = "Japão é o melhor time"
-
-alert(frase.replace("Japão", "Brasil"))
-alert(frase.toLowerCase())
